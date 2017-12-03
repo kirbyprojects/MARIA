b90a3755db4785161ab116ed80f51ef952270092
shawn25n92 <shawn25n92@gmail.com>
2017-12-03 19:45:30 +0800

    Runtime Logic File

--- MARIA/Program.cs
+++ MARIA/Program.cs
@@ -11,21 +11,66 @@ namespace MARIA
     {
         static void Main(string[] args)
         {
-            /*HttpClient ApplicationHttpClient = new HttpClient();
-            MARIAWebRequest TargetSite = new MARIAWebRequest("https://indonesia.merimen.com/claims/index.cfm?fusebox=MTRreportsI&fuseaction=dsp_custom&RPTNAME=dsp_pivotmain.cfm&ProcessRpt=1&dl=0&CFID=3322076&CFTOKEN=ce895f2b64d6d32f-D20F34A6-155D-B8AC-03500B0FF3A1FD0E&USID=8189&RID=1313732&BR=700513*&DRFROM=01%2F12%2F2017&DRTO=02%2F12%2F2017&DATEBY=1");
-            TargetSite.Get(ApplicationHttpClient);
-            MARIAWebRequest TargetSite2 = new MARIAWebRequest("");
-            TargetSite2.AddPostParameter("hello", "world");
-            TargetSite2.Post(ApplicationHttpClient);*/
-            /*MARIAFile TargetFile = new MARIAFile(@"D:\shawn.txt");
-            TargetFile.ReadText(TextReadMode.READLINE, MARIAFile.AnalyzeRequest);
-            Console.WriteLine(TargetFile.FileType);*/
+            bool ProgramRunning = true;
+            string StartupMode = "console";
+            string UserInput;
+            MARIAFile ActiveFile = null;
+            while (ProgramRunning)
+            {
+                try
+                {
+                    Console.Write("Command: ");
+                    UserInput = Console.ReadLine();
+                    string PrimaryCommand = UserInput.Split(' ')[0].Trim();
+                    string InstructionSet = UserInput.Remove(0, PrimaryCommand.Length).Trim();
+                    if(UserInput != "exit")
+                    {
+                        if(PrimaryCommand == "file")
+                        {
+                            if(ActiveFile != null)
+                            {
+                                string SecondaryCommand = InstructionSet.Split(' ')[0];
+                                if(SecondaryCommand == "open")
+                                {
+                                    string FilePath;
+                                    Console.Write("Set Active File: ");
+                                    FilePath = Console.ReadLine();
+                                    ActiveFile = new MARIAFile(FilePath);
+                                }
+                                else if (SecondaryCommand == "imageread")
+                                {
+                                    ActiveFile.ReadImage();
+                                }
+                                else if (SecondaryCommand == "clear")
+                                {
+                                    ActiveFile = null;
+                                }
+                                else
+                                {
 
-            /*MARIACryptography CryptoTools = new MARIACryptography();
-            Console.WriteLine(CryptoTools.GetHash(HashAlgorithm.MD5, "MARINES_54"));*/
-            MARIAFile ImageFile = new MARIAFile(@"C:\Users\User-G56J\Pictures\Screenshots\Screenshot (1543).png");
-            MARIAFile ImageFile2 = new MARIAFile(@"C:\Users\User-G56J\Desktop\cute vids for dyana\tumblr_oss7sq7pkg1rhrsjdo2_540.png");
-            ImageFile2.ReadImage();
+                                }
+                            }
+                            else
+                            {
+                                string FilePath;
+                                Console.Write("Set Active File: ");
+                                FilePath = Console.ReadLine();
+                                ActiveFile = new MARIAFile(FilePath);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ProgramRunning = false;
+                        UserInput = null;
+                    }
+                }
+                catch(Exception e)
+                {
+                    ProgramRunning = false;
+                    UserInput = null;
+                }
+            }
         }
     }
 }
