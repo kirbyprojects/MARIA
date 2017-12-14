bf8bdd2b96e39c950f60a93b8c16ed3662378461
Shawn Invento <shawn25n92github@gmail.com>
2017-12-14 18:49:32 +0800

    #10

---
+++ MARIA/MARIAProcess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MARIA
+{
+    public partial class MARIAProcess
+    {
+        public string ProcessName { get; private set; }
+        public MARIAProcess(string ProcessName)
+        {
+            this.ProcessName = ProcessName;
+        }
+        
+        public StatusObject Start()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+                Process.Start(this.ProcessName);
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+        public StatusObject GetProcessFiles()
+        {
+            StatusObject SO = new StatusObject();
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+
+            }
+            return SO;
+        }
+    }
+}
